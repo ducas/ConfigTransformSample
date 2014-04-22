252412600d84fa7ad8170c80dff5b961f35eafb4
Ducas Francis <ducas@ducasfrancis.com>
2014-04-22 14:18:50 +1000

    Add environment name configuration variable and display on home page

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace Web.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.EnvironmentName = ConfigurationManager.AppSettings["EnvironmentName"];
             var lastPing = HttpContext.Application["last-ping"];
             if (lastPing == null)
             {
