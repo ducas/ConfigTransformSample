2c64b3566b309ff713cbf39da12795f231d0fc65
Ducas Francis <ducas@ducasfrancis.com>
2014-04-22 14:09:02 +1000

    Add ping web and job projects

---
+++ Job/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Threading;
+
+namespace Job
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                var url = ConfigurationManager.AppSettings["url"];
+                Console.WriteLine("{0} - Pinging {1}", DateTime.UtcNow, url);
+                var client = WebRequest.CreateHttp(url);
+                try
+                {
+                    using (var r = (HttpWebResponse)client.GetResponse())
+                    {
+                        Console.WriteLine("{0} - Returned status {1}", DateTime.UtcNow, r.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0} - Something went wrong - {1}", DateTime.UtcNow, ex);
+                }
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Parser;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var lastPing = HttpContext.Application["last-ping"];
+            if (lastPing == null)
+            {
+                ViewBag.LastPing = "never";
+            }
+            else
+            {
+                ViewBag.LastPing = ((DateTime) lastPing) + " UTC";
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Ping()
+        {
+            HttpContext.Application["last-ping"] = DateTime.UtcNow;
+            return new EmptyResult();
+        }
+    }
+}
